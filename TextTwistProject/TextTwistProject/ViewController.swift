612b301f39569426744dcfd8dbf7a121c3fe6eff
Ian Bailey <iankbailey2@gmail.com>
2019-11-13 14:58:02 -0500

    check func and basic logic down

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -11,19 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet var background: UIView!
     @IBOutlet weak var randomWordLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
+ 
+    
+    let selectedWord = Word.getRandomWord()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        randomWordLabel.text = Word.getRandomWord().scrambled
+        randomWordLabel.text = selectedWord.scrambled
+        textField.delegate = self
+    }
+    
+    private func checkAnswer() {
+        let guessed = (textField.text ?? "").lowercased()
+        let userInput = Set(guessed)
+        let correctAnswer = Set(selectedWord.unscrambled)
         
+        if userInput.isSubset(of: correctAnswer) {
+            if guessed.lowercased() == selectedWord.unscrambled {
+                background.backgroundColor = .green
+            }
+            } else {
+                background.backgroundColor = .red
+        }
     }
 
-    
-    
     
 
     @IBAction func resetButton(_ sender: UIButton) {
@@ -35,3 +51,18 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        for char in Word.randomWordArr(word: selectedWord) {
+//            if textField.text == String(char) {
+//
+//            }
+//        }
+//    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        checkAnswer()
+        textField.resignFirstResponder()
+        return true
+    }
+}
--- TextTwistProject/TextTwistProject/Word.swift
+++ TextTwistProject/TextTwistProject/Word.swift
@@ -18,4 +18,17 @@ struct Word {
             Word(unscrambled: "fixing")
         ].randomElement()!
     }
+    
+    static func randomWordArr(word: String) -> [Character] {
+        var arr = [Character]()
+        
+        for char in word {
+            arr.append(char)
+        }
+        return arr
+    }
+    
+    
+    
+    
 }
